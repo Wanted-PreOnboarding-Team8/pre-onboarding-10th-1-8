a08abf3999d5aa092a0298f30009d840fb65b627
SangDon <adoim@naver.com>
2023-04-27 20:19:36 +0900

    Feat: TodoApi 인스턴스 생성

---
+++ src/api/TodoApi.tsx
@@ -0,0 +1,20 @@
+import axios, { type InternalAxiosRequestConfig } from 'axios';
+import BASE_URL from './constant';
+
+const TodoApi = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+TodoApi.interceptors.request.use((request: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem('access_token');
+
+  if (accessToken !== null) {
+    request.headers.authorization = `bearer ${accessToken}`;
+  }
+  return request;
+});
+
+export default TodoApi;
