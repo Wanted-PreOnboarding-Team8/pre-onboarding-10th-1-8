fac05899d728e8fd7fcc6bb47d37d128de6c554d
shaqok <shaqok8@gmail.com>
2023-04-28 21:47:58 +0900

    Style: Object.freeze로 변경 불가하게 수정
    Co-authored-by: 조승현 <tmdgus95@users.noreply.github.com>
    Co-authored-by: eunyeong <jjangeunyeong@users.noreply.github.com>
    Co-authored-by: powercording <powercording@users.noreply.github.com>
    Co-authored-by: kyhui1115 <kyhui1115@users.noreply.github.com>
    Co-authored-by: Sheryl Yena Yun <cheryleduart@gmail.com>
    Co-authored-by: bicco2 <asxws7941@gmail.com>

--- src/hooks/const/index.ts
+++ src/hooks/const/index.ts
@@ -1,7 +1,11 @@
-const STATUS = {
+const STATUS = Object.freeze({
   OK: 200,
   CREATED: 201,
   NO_CONTENT: 204,
-};
+});
+
+export const API = Object.freeze({
+  TODOS: 'todos',
+});
 
 export default STATUS;
