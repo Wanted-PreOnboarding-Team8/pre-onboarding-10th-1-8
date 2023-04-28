a625f21ed5f90536034dc76cd995de16a08113fd
shaqok <shaqok8@gmail.com>
2023-04-28 22:36:06 +0900

    Fix: 토큰을 가지고 root path로 이동할 수 있도록 라우터 수정
    Co-authored-by: 조승현 <tmdgus95@users.noreply.github.com>
    Co-authored-by: eunyeong <jjangeunyeong@users.noreply.github.com>
    Co-authored-by: powercording <powercording@users.noreply.github.com>
    Co-authored-by: kyhui1115 <kyhui1115@users.noreply.github.com>
    Co-authored-by: Sheryl Yena Yun <cheryleduart@gmail.com>
    Co-authored-by: bicco2 <asxws7941@gmail.com>

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Home from './pages/Home/Home';
 import Todo from './pages/Todo';
 import NotFound from './pages/NotFound';
 import PublicRoute from './components/route/PublicRoute';
@@ -11,8 +11,8 @@ import ProtectedRoute from './components/route/ProtectedRoute';
 export default function Router() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route element={<PublicRoute />}>
-        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Register />} />
       </Route>
