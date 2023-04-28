aecebafc4be38208d76d5311262aaf253d4835f4
SangDon <adoim@naver.com>
2023-04-28 11:25:57 +0900

    Style: 글로벌 스타일 flexBox 로 정의

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -6,6 +6,11 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+     height: 100dvh;
   }
 `;
 
